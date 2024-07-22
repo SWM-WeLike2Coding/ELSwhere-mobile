bd9b92809553b1bc0a9029a37f81aa392389910f
조성범 <joseongbeom@joseongbeom-ui-MacBookPro.local>
2024-07-22 20:38:21 +0900

    feat: 상품 검색 기능 추가

--- lib/widgets/search_text_field.dart
+++ lib/widgets/search_text_field.dart
@@ -1,15 +1,57 @@
 import 'dart:ui';
 
 import 'package:elswhere/resources/app_resource.dart';
+import 'package:elswhere/views/detail_search_modal.dart';
 import 'package:flutter/material.dart';
 
-class SearchTextField extends StatelessWidget {
+import '../views/alarm_setting_modal.dart';
+
+class SearchTextField extends StatefulWidget {
+  const SearchTextField({super.key});
+
+  @override
+  State<SearchTextField> createState() => _SearchTextFieldState();
+}
+
+class _SearchTextFieldState extends State<SearchTextField> {
+  late FocusNode _focusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = FocusNode();
+
+    // Add listener to show modal when the TextField gains focus
+    _focusNode.addListener(() {
+      if (_focusNode.hasFocus) {
+        _showModal();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  void _showModal() {
+    _focusNode.unfocus();
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      useSafeArea: true,
+      builder: (context) => const DetailSearchModal(),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+
     return Container(
       height: 50,
-      child: const TextField(
+      child: TextField(
+        focusNode: _focusNode,
         decoration: InputDecoration(
           focusColor: Colors.amber,
           prefixIcon: Icon(Icons.search),
@@ -24,5 +66,15 @@ class SearchTextField extends StatelessWidget {
         ),
       ),
     );
+
+
+    // return TextField(
+    //   focusNode: _focusNode,
+    //   decoration: InputDecoration(
+    //     labelText: 'Search',
+    //     border: OutlineInputBorder(),
+    //   ),
+    // );
   }
 }
+
