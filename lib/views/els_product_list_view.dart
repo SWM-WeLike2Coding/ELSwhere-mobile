deeb3041c42ec3095cf9780033d3e1b5ab47b216
성필 <philip000412@gmail.com>
2024-07-24 18:28:06 +0900

    fix: 상품 목록 리스트 위젯 빌드 도중에 setState() 호출되는 문제 해결

--- lib/views/els_product_list_view.dart
+++ lib/views/els_product_list_view.dart
@@ -8,11 +8,16 @@ class ELSProductListView extends StatelessWidget {
 
   final String type;
 
+  void _refreshList(BuildContext context) {
+    Provider.of<ELSProductsProvider>(context, listen: false).refreshProducts(type);
+  }
+
   @override
   Widget build(BuildContext context) {
+    print(type);
     return Expanded(
       child: FutureBuilder(
-        future: Provider.of<ELSProductsProvider>(context, listen: false).refreshProducts(type),
+        future: Provider.of<ELSProductsProvider>(context, listen: false).initProducts(type),
         builder: (ctx, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
@@ -21,16 +26,12 @@ class ELSProductListView extends StatelessWidget {
           } else {
             return Consumer<ELSProductsProvider>(
               builder: (context, productsProvider, child) {
-                Widget body;
-                if (productsProvider.isLoading &&
-                    productsProvider.products.isEmpty) {
-                  body = const Center(child: CircularProgressIndicator());
-                }
-                else if (productsProvider.products.isEmpty) {
-                  body = const Center(child: Text('상품이 존재하지 않습니다.'));
-                }
-                else {
-                  body = NotificationListener<ScrollNotification>(
+                if (productsProvider.isLoading && productsProvider.products.isEmpty) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (productsProvider.products.isEmpty) {
+                  return const Center(child: Text('상품이 존재하지 않습니다.'));
+                } else {
+                  return NotificationListener<ScrollNotification>(
                     onNotification: (ScrollNotification scrollInfo) {
                       if (!productsProvider.isLoading &&
                           productsProvider.hasNext &&
@@ -40,23 +41,22 @@ class ELSProductListView extends StatelessWidget {
                       }
                       return false;
                     },
-                    child: ListView.builder(
-                      itemCount: productsProvider.products.length,
-                      itemBuilder: (context, index) {
-                        return ELSProductCard(
-                          product: productsProvider.products[index],
-                          index: index,
-                        );
+                    child: RefreshIndicator(
+                      onRefresh: () async {
+                        _refreshList(context);
                       },
+                      child: ListView.builder(
+                        itemCount: productsProvider.products.length,
+                        itemBuilder: (context, index) {
+                          return ELSProductCard(
+                            product: productsProvider.products[index],
+                            index: index,
+                          );
+                        },
+                      ),
                     ),
                   );
                 }
-                return RefreshIndicator(
-                  onRefresh: () async {
-                    productsProvider.refreshProducts(type);
-                  },
-                  child: body,
-                );
               },
             );
           }
