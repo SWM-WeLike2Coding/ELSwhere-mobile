362de3dd2f46463bc782072d225c1720ca89e775
조성범 <joseongbeom@joseongbeom-ui-MacBookPro.local>
2024-08-08 19:38:07 +0900

    feat: 피드백 받는 구글 폼 추가

--- lib/data/models/dtos/response_login_dto.g.dart
+++ lib/data/models/dtos/response_login_dto.g.dart
@@ -8,12 +8,12 @@ part of 'response_login_dto.dart';
 
 ResponseLoginDto _$ResponseLoginDtoFromJson(Map<String, dynamic> json) =>
     ResponseLoginDto(
-      accessToken: json['accessToken'] as String,
-      refreshToken: json['refreshToken'] as String,
+      accessToken: json['access_token'] as String,
+      refreshToken: json['refresh_token'] as String,
     );
 
 Map<String, dynamic> _$ResponseLoginDtoToJson(ResponseLoginDto instance) =>
     <String, dynamic>{
-      'accessToken': instance.accessToken,
-      'refreshToken': instance.refreshToken,
+      'access_token': instance.accessToken,
+      'refresh_token': instance.refreshToken,
     };
