a52cf5e46deeb3dd61d0860dda718ee9714a11bc
성필 <philip000412@gmail.com>
2024-11-04 16:20:35 +0900

    hotfix: postclone, prebuild 수정

--- ios/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -2,6 +2,8 @@
 
 echo "Setting up build for the correct flavor"
 
+export PATH="$HOME/flutter/bin:$PATH"
+
 # Xcode 스킴에 따라 플래버를 설정
 if [ "$CI_XCODE_SCHEME" = "prod" ]; then
     echo "Building for production"
