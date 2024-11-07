485eb0c04505fba131cbff109d61358fb3b64600
성필 <philip000412@gmail.com>
2024-11-07 19:45:32 +0900

    fix: 주가 지수 그래프에서 상장 시기가 다른 경우 데이터 보정

--- lib/ui/widgets/stock_price_graph.dart
+++ lib/ui/widgets/stock_price_graph.dart
@@ -3,7 +3,6 @@ import 'dart:math';
 import 'package:elswhere/config/app_resource.dart';
 import 'package:elswhere/data/models/stock_price.dart';
 import 'package:elswhere/data/providers/els_product_provider.dart';
-import 'package:elswhere/ui/screens/more/more_screen.dart';
 import 'package:elswhere/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -23,9 +22,12 @@ class StockPriceGraph extends StatefulWidget {
 class _StockPriceGraphState extends State<StockPriceGraph> {
   int selectedPeriod = 0;
   List<List<FlSpot>> stockData = [];
+  List<Duration> intervalDurations = const [Duration(days: 5), Duration(days: 15), Duration(days: 31), Duration(days: 61), Duration(days: 182), Duration(days: 305)];
   final DateFormat format = DateFormat().addPattern('yyyy년 MM월 dd일');
   final DateTime now = DateTime.now().getJustDay();
   Map<String, List<StockPrice>>? prices;
+  late DateTime startDate;
+  late DateTime endDate;
 
   // 기간별 데이터 불러오기
   void fetchStockData(int period, Map<String, YahooFinanceResponse> response) {
@@ -36,6 +38,8 @@ class _StockPriceGraphState extends State<StockPriceGraph> {
     DateTime threeYearsAgo = DateTime(now.year - 3, now.month, now.day);
     DateTime fiveYearsAgo = DateTime(now.year - 5, now.month, now.day);
 
+    endDate = now;
+
     prices = response.map((equity, yResponse) {
       final convert = yResponse.candlesData.map((e) => StockPrice(date: e.date, price: e.close)).toList();
       return MapEntry(equity, convert);
@@ -43,62 +47,56 @@ class _StockPriceGraphState extends State<StockPriceGraph> {
 
     switch (period) {
       case 0:
-        prices = prices?.map((equity, stockPrice) {
-          final location = stockPrice.indexWhere((element) => isDateMatchingWithPastPeriods(element.date, oneMonthAgo));
-          stockPrice = stockPrice.sublist(location);
-          return MapEntry(equity, stockPrice);
-        });
+        startDate = oneMonthAgo;
         break;
       case 1:
-        prices = prices?.map((equity, stockPrice) {
-          final location = stockPrice.indexWhere((element) => isDateMatchingWithPastPeriods(element.date, threeMonthsAgo));
-          stockPrice = stockPrice.sublist(location);
-          return MapEntry(equity, stockPrice);
-        });
+        startDate = threeMonthsAgo;
         break;
       case 2:
-        prices = prices?.map((equity, stockPrice) {
-          final location = stockPrice.indexWhere((element) => isDateMatchingWithPastPeriods(element.date, sixMonthsAgo));
-          stockPrice = stockPrice.sublist(location);
-          return MapEntry(equity, stockPrice);
-        });
+        startDate = sixMonthsAgo;
         break;
       case 3:
-        prices = prices?.map((equity, stockPrice) {
-          final location = stockPrice.indexWhere((element) => isDateMatchingWithPastPeriods(element.date, oneYearAgo));
-          stockPrice = stockPrice.sublist(location);
-          return MapEntry(equity, stockPrice);
-        });
+        startDate = oneYearAgo;
         break;
       case 4:
-        prices = prices?.map((equity, stockPrice) {
-          final location = stockPrice.indexWhere((element) => isDateMatchingWithPastPeriods(element.date, threeYearsAgo));
-          stockPrice = stockPrice.sublist(location);
-          return MapEntry(equity, stockPrice);
-        });
+        startDate = threeYearsAgo;
         break;
       case 5:
-        prices = prices?.map((equity, stockPrice) {
-          final location = stockPrice.indexWhere((element) => isDateMatchingWithPastPeriods(element.date, fiveYearsAgo));
-          stockPrice = stockPrice.sublist(location);
-          return MapEntry(equity, stockPrice);
-        });
+        startDate = fiveYearsAgo;
+        break;
+      default:
+        startDate = fiveYearsAgo;
         break;
     }
+
+    prices = prices?.map((equity, stockPrice) {
+      final location = stockPrice.indexWhere((element) => isDateMatchingWithPastPeriods(element.date.getJustDay(), startDate));
+      stockPrice = stockPrice.sublist(location);
+      return MapEntry(equity, stockPrice);
+    });
 
     stockData = prices!.values.map((prices) {
       return convertStockDataToRelativeFlSpots(prices.toList());
     }).toList();
+
+    final maxLength = stockData.map((e) => e.length).reduce(max);
+    for (int i = 0; i < prices!.length; i++) {
+      int difference = maxLength - stockData[i].length;
+      for (int j = 0; j < difference; j++) {
+        stockData[i].insert(0, FlSpot.nullSpot);
+      }
+    }
   }
 
   // 주가 데이터를 상대적인 퍼센트 값으로 변환
   List<FlSpot> convertStockDataToRelativeFlSpots(List<StockPrice> stockData) {
     final double startPrice = stockData.first.price;
-    final DateTime startDate = stockData.first.date;
+    final DateTime stockStartDate = stockData.first.date.getJustDay();
+    final double difference = stockStartDate.difference(startDate).inDays.toDouble();
     int cnt = 0;
 
     return stockData.map((data) {
-      final double x = (data.date.difference(startDate).inDays.toDouble());
+      final double x = (data.date.getJustDay().difference(stockStartDate).inDays.toDouble()) + difference;
       final double y = cnt++ == 0 ? 100 : (data.price / startPrice) * 100; // 시작 값을 100%로 설정
       return FlSpot(x, y);
     }).toList();
@@ -108,14 +106,20 @@ class _StockPriceGraphState extends State<StockPriceGraph> {
     return !date.isBefore(dateToCompare);
   }
 
+  double findMinX(List<List<FlSpot>> stockData) {
+    return stockData.expand((data) => data).map((data) => data.x).where((e) => e.isFinite).reduce(min);
+  }
+
+  double findMaxX(List<List<FlSpot>> stockData) {
+    return stockData.expand((data) => data).map((data) => data.x).where((e) => e.isFinite).reduce(max);
+  }
+
   double findMinY(List<List<FlSpot>> stockData) {
-    final result = stockData.expand((data) => data).map((data) => data.y).reduce((a, b) => a < b ? a : b);
-    return result;
+    return stockData.expand((data) => data).map((data) => data.y).where((e) => e.isFinite).reduce(min);
   }
 
   double findMaxY(List<List<FlSpot>> stockData) {
-    final result = stockData.expand((data) => data).map((data) => data.y).reduce((a, b) => a > b ? a : b);
-    return result;
+    return stockData.expand((data) => data).map((data) => data.y).where((e) => e.isFinite).reduce(max);
   }
 
   LineChartData getMultiLineChartData(Map<String, List<StockPrice>> stockDataMap) {
@@ -134,11 +138,16 @@ class _StockPriceGraphState extends State<StockPriceGraph> {
       );
     }).toList();
 
+    double minX = findMinX(stockData);
+    double maxX = findMaxX(stockData);
+
     double minY = findMinY(stockData);
     double maxY = findMaxY(stockData);
     double padding = (maxY - minY) * 0.2; // 20% 여유를 둠
 
     return LineChartData(
+      minX: minX,
+      maxX: maxX,
       minY: minY - padding,
       maxY: maxY + padding,
       gridData: const FlGridData(show: true),
@@ -150,37 +159,45 @@ class _StockPriceGraphState extends State<StockPriceGraph> {
         bottomTitles: AxisTitles(
           sideTitles: SideTitles(
             reservedSize: 35,
-            interval: <double>[10.5, 31, 63, 365 / 3, 365, 365 * 5 / 3][selectedPeriod],
+            interval: (maxX - minX) / 3,
             showTitles: true,
             getTitlesWidget: (value, meta) {
-              DateTime date = stockDataMap.values.first.first.date.add(Duration(days: value.toInt()));
-              return Padding(
-                padding: const EdgeInsets.only(top: 8),
-                child: RichText(
-                  textAlign: TextAlign.center,
-                  text: TextSpan(
-                    children: [
-                      TextSpan(
-                        text: '${DateFormat().addPattern('yyyy년').format(date)}\n',
-                        style: textTheme.labelSmall!.copyWith(color: Colors.black),
-                      ),
-                      TextSpan(
-                        text: DateFormat().addPattern('MM월 dd일').format(date),
-                        style: textTheme.labelSmall!.copyWith(color: Colors.black),
-                      ),
-                    ],
+              DateTime date = stockDataMap.values.first.first.date.add(Duration(days: value.toInt())).getJustDay();
+              DateTime minDate = stockDataMap.values.first.first.date.getJustDay();
+              DateTime maxDate = stockDataMap.values.last.last.date.getJustDay();
+
+              if (value == minX || value == maxX || (minDate.difference(date).abs().inDays > (maxX - minX) / 6 && maxDate.difference(date).abs().inDays > (maxX - minX) / 6)) {
+                return Padding(
+                  padding: const EdgeInsets.only(top: 8),
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: '${DateFormat().addPattern('yyyy년').format(date)}\n',
+                          style: textTheme.labelSmall!.copyWith(color: Colors.black),
+                        ),
+                        TextSpan(
+                          text: DateFormat().addPattern('MM월 dd일').format(date),
+                          style: textTheme.labelSmall!.copyWith(color: Colors.black),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              );
+                );
+              } else {
+                return const SizedBox.shrink();
+              }
             },
           ),
         ),
         leftTitles: AxisTitles(
           sideTitles: SideTitles(
-              reservedSize: 50,
-              interval: (maxY - minY + padding * 2) / 3,
-              showTitles: true,
-              getTitlesWidget: (value, meta) {
+            reservedSize: 55,
+            interval: (maxY - minY + padding * 2) / 3,
+            showTitles: true,
+            getTitlesWidget: (value, meta) {
+              if (value - minY + padding < 0.01 || maxY + padding - value < 0.01 || ((value - minY).abs() > padding && (maxY - value).abs() > padding)) {
                 return Padding(
                   padding: const EdgeInsets.only(right: 8),
                   child: Text(
@@ -188,7 +205,11 @@ class _StockPriceGraphState extends State<StockPriceGraph> {
                     style: textTheme.labelSmall,
                   ),
                 );
-              }),
+              } else {
+                return const SizedBox.shrink();
+              }
+            },
+          ),
         ),
       ),
       lineTouchData: LineTouchData(
@@ -204,6 +225,8 @@ class _StockPriceGraphState extends State<StockPriceGraph> {
               final index = touchedSpots.indexOf(touchedSpot);
               final equity = prices!.keys.toList()[index];
               final color = [Colors.red, Colors.blue, Colors.green, Colors.yellow][index];
+              double spotY = lineBarsData[stockIndex].spots[touchedSpot.spotIndex].y;
+              if (spotY.isNaN) spotY = 100.0;
 
               final item = LineTooltipItem(
                 '',
@@ -219,7 +242,8 @@ class _StockPriceGraphState extends State<StockPriceGraph> {
                       ),
                     ),
                   TextSpan(
-                    text: '$equity:\n${lineBarsData[stockIndex].spots[min(touchedSpot.spotIndex, lineBarsData[stockIndex].spots.length - 1)].y.toStringAsFixed(2)}%',
+                    text: '$equity:\n${spotY.toStringAsFixed(2)}%',
+                    // text: '$equity:\n${touchedSpot.y.toStringAsFixed(2)}%',
                     style: TextStyle(
                       color: color,
                       fontWeight: FontWeight.bold,
@@ -233,6 +257,22 @@ class _StockPriceGraphState extends State<StockPriceGraph> {
             }).toList();
           },
         ),
+      ),
+      extraLinesData: ExtraLinesData(
+        extraLinesOnTop: false,
+        horizontalLines: [
+          HorizontalLine(
+            y: 100.0,
+            color: AppColors.gray500,
+            dashArray: [8, 4],
+            // label: HorizontalLineLabel(
+            //   labelResolver: (p0) => '100.0%',
+            //   show: true,
+            //   alignment: Alignment.centerRight,
+            //   style: textTheme.SM_12,
+            // ),
+          ),
+        ],
       ),
     );
   }
